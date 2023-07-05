b38fab4b73619f912978ea3e3e005f3108b0a5d9
Zachery Vitko <Zwvitko1@gmail.com>
2023-07-04 21:40:49 -0400

    pushing

--- app.js
+++ app.js
@@ -1,39 +1,54 @@
-window.addEventListener('load', () => {
-    const weatherInfo = document.getElementById('weather-info');
-    const locationInput = document.getElementById('location-input');
-    const submitBtn = document.getElementById('submit-btn');
-  
-    // Function to fetch weather data from the API
-    const fetchWeatherData = async (location) => {
-      const apiKey = process.env.API_KEY;
-      const apiUrl = 'https://api.weatherapi.com/v1/current.json';
-  
-      try {
-        const response = await fetch(`${apiUrl}?key=${apiKey}&q=${location}`);
-        const data = await response.json();
-  
-        // Extract relevant weather information from the API response
-        const temperature = data.current.temp_c;
-        const condition = data.current.condition.text;
-  
-        // Display the weather information
-        weatherInfo.innerHTML = `
-          <h2>Current Weather in ${location}</h2>
-          <p>Temperature: ${temperature}°C</p>
-          <p>Condition: ${condition}</p>
-        `;
-      } catch (error) {
-        console.log('Error fetching weather data:', error);
-        weatherInfo.innerHTML = '<h2>Unable to fetch weather data</h2>';
-      }
-    };
-  
-    // Handle the button click event
-    submitBtn.addEventListener('click', () => {
-      const location = locationInput.value.trim();
-      if (location !== '') {
-        fetchWeatherData(location);
-      }
-    });
-  });
-  
+// Import the dotenv package
+require('dotenv').config();
+
+const locationInput = document.querySelector("#location-input");
+const submitButton = document.querySelector("#submit-btn");
+const weatherInfo = document.querySelector("#weather-info");
+const loadingDiv = document.querySelector("#loading");
+const errorDiv = document.querySelector("#error");
+
+submitButton.addEventListener("click", function() {
+  fetchWeatherData(locationInput.value);
+});
+
+// Function to fetch weather data from the API
+const fetchWeatherData = async (location) => {
+  const apiKey = process.env.API_KEY;
+  const apiUrl = 'https://api.weatherapi.com/v1/current.json';
+
+  // Show loading state
+  loadingDiv.style.display = 'block';
+  weatherInfo.style.visibility = 'hidden';
+  errorDiv.style.display = 'none';
+
+  try {
+    const response = await fetch(`${apiUrl}?key=${apiKey}&q=${location}`);
+
+    // Hide loading state
+    loadingDiv.style.display = 'none';
+
+    if (response.ok) {
+      const data = await response.json();
+
+      // Extract relevant weather information from the API response
+      const temperature = data.current.temp_c;
+      const condition = data.current.condition.text;
+
+      // Display the weather information
+      weatherInfo.style.visibility = 'visible';
+      weatherInfo.innerHTML = `
+        <h2>Current Weather in ${location}</h2>
+        <p>Temperature: ${temperature}°C</p>
+        <p>Condition: ${condition}</p>
+      `;
+    } else {
+      throw new Error('Unable to fetch the weather data');
+    }
+  } catch (error) {
+    errorDiv.style.display = 'block';
+    errorDiv.textContent = 'Error fetching weather data: ' + error.message;
+    console.log('Error fetching weather data:', error);
+  }
+};
+
+// No need to export in a browser environment
